2c3a22cfcb984e4b1f42e23203b46d6fb130ee80
Alisson Cruz <alissonpdc@gmail.com>
2024-01-16 14:52:12 -0300

    add: 20-time-sleep

---
+++ 20-time-sleep/bucket.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "terraform-for-beginners-demo-first-bucket"
+}
+
+# Start a 'timer sleep' after first bucket has been provisioned
+resource "time_sleep" "sleep_30s" {
+  depends_on = [ 
+    aws_s3_bucket.first_bucket 
+  ]
+
+  # Create: First Bucket => wait 30s => Second Bucket
+  # Destroy: First Bucket => wait 60s => Second Bucket
+  create_duration = "30s"
+  destroy_duration = "60s"
+}
+
+# Create second bucket only after 'timer sleep' has finished
+resource "aws_s3_bucket" "second_bucket" {
+  depends_on = [ 
+    time_sleep.sleep_30s 
+  ]
+  
+  bucket = "terraform-for-beginners-demo-second-bucket"
+}
