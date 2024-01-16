625776e452d80984f8031b2ebb00d3996c92f6da
Alisson Cruz <alissonpdc@gmail.com>
2024-01-16 14:34:45 -0300

    fix: output arn/id

--- 12-count/outputs.tf
+++ 12-count/outputs.tf
@@ -2,15 +2,15 @@
 
 # arn from subnet-0: 10.10.0.0/24
 output "subnet_id_0" {
-  value = aws_subnet.subnet[0].arn
+  value = aws_subnet.subnet[0].id
 }
 
 # arn from subnet-1: 10.10.1.0/24
 output "subnet_id_1" {
-  value = aws_subnet.subnet[1].arn
+  value = aws_subnet.subnet[1].id
 }
 
 # arn from subnet-2: 10.10.2.0/24
 output "subnet_id_2" {
-  value = aws_subnet.subnet[2].arn
+  value = aws_subnet.subnet[2].id
 }
